86fba26ef0aecc534d3c29fc9b4f626142f8b4bc
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2025-06-04 08:13:36 -0300

    Update script.js

--- computador/script.js
+++ computador/script.js
@@ -1,28 +1,50 @@
-import {copiar_ncm} from './ncm.js'
-
-let soma = 0;
-
+import { copiar_ncm } from './ncm.js'
 
 //Quando Clicado TAB e esteja no COD. DO MATERIAL, pula direto para o PESO DO MATERIAL
-document.addEventListener('keydown', (event) =>{
+document.addEventListener('keydown', (event) => {
     if (document.activeElement.id === 'select' && event.key === 'Tab') {
         document.getElementById('limpar').select();
-    }   
-    
+        document.getElementById('quantidade_soma').select();
+        document.getElementById('_calc').select();
+    }
+
 });
+
+document.addEventListener('keydown', (event) => {
+    if (document.activeElement.id === 'quantidade_soma' && event.key === 'Tab') {
+        document.getElementById('_calc').select();
+    }
+
+});
+
+
 
 //ATIVIDA AO CLICAR NO BOTAO
 document.getElementById('calcular').addEventListener('click', () => calcular());
 
 // ATIVA AO CLICAR NA TECLA 'ENTER'
 document.body.addEventListener('keypress', (event) => {
-        if(event.key == 'Enter'){
-            calcular();
-        }
+    if (event.key == 'Enter') {
+        calcular();
+    }
 });
 
 
-    function calcular(){
+let tabel = true;
+document.getElementById('botao').addEventListener('click', () => {
+    if (tabel == false) {
+        document.getElementById('tabela').innerHTML = " ";
+
+        document.getElementById('botao').value = "ABRIR";
+    } else {
+        document.getElementById('tabela').innerHTML = "<tr><td><strong>LETRA</strong></td><td><strong>CATEGORIA</strong></td><td><strong>NCM</strong></td></tr><tr><td><strong>A</strong></td><td>BUCHA BRONZE</td><td>74072921</td></tr><tr><td><strong>B</strong></td><td>TARUGO DE BRONZE</td><td>74072910</td></tr><tr><td><strong>C</strong></td><td>BARRA LATAO</td><td>74072110</td></tr><tr><td><strong>D</strong></td><td>CHAPA LATAO</td><td>74092900</td></tr><tr><td><strong>F</strong></td><td>BARRA COBRE</td><td>74071010</td></tr><tr><td><strong>G</strong></td><td>CHAPA COBRE</td><td>74091900</td></tr><tr><td><strong>H</strong></td><td>TUBO COBRE</td><td>74111010</td></tr><tr><td><strong>I</strong></td><td>BARRA ALUMINIO</td><td>76042919</td></tr><tr><td><strong>J</strong></td><td>CHAPA ALUMINIO</td><td>76061290</td></tr><tr><td><strong>L</strong></td><td>TARUGO ALUMINIO</td><td>76012000</td></tr><tr><td><strong>K</strong></td><td>TUBO ALUMINIO</td><td>76082090</td></tr><tr><td><strong>M</strong></td><td>CHAPA INOX</td><td>72193300</td></tr><tr><td><strong>N</strong></td><td>BARRA INOX</td><td>72221100</td></tr><tr><td><strong>O</strong></td><td>CHUMBO BRIDGESTONE</td><td>78011011</td></tr>";
+        document.getElementById('botao').value = 'ESCONDER';
+    }
+    tabel = !tabel; //alterna o valor entre verdadiro e falso
+
+});
+
+function calcular() {
 
     // VARIAVEIS
     let select = document.getElementById('select').value;
@@ -32,7 +54,6 @@ document.body.addEventListener('keypress', (event) => {
     let ipi_input = document.getElementById('ipi');
     let icms_input = document.getElementById('icms');
     let total = document.getElementById('total');
-    let soma_final = document.getElementById('soma_f');
     let reducao = document.getElementById('reducao');
     let PIS = document.getElementById('pis');
     let COFINS = document.getElementById('cofins');
@@ -60,7 +81,6 @@ document.body.addEventListener('keypress', (event) => {
     let cofins = (re_pis_cofins * 0.03).toFixed(2);
 
 
-    soma += peso_sem_ipi; 
 
     // SAÍDA
 
@@ -72,9 +92,9 @@ document.body.addEventListener('keypress', (event) => {
         reducao.value = re_pis_cofins;
         PIS.value = pis;
         COFINS.value = cofins;
-        soma_final.value =  soma.toFixed(2);
+        calcular_final();
 
-        if (select == '02') {   
+        if (select == '02') {
             ipi_input.value *= 0;
             total.value = parseFloat(peso_sem_ipi).toFixed(2);
 
@@ -93,10 +113,10 @@ document.body.addEventListener('keypress', (event) => {
 
 
 
-    }
+}
 
 //COPIAR O NCM
-    copiar_ncm(); 
+copiar_ncm();
 
 // LIMPAR 
 let limpar = document.getElementById('limpar');
@@ -107,32 +127,74 @@ limpar.addEventListener('click', () => {
     window.location.reload();
 
 });
-limpar.addEventListener('dblclick', () => {
-    document.getElementById('soma_f').value = '';
-    soma -= soma;
-});
-//LIMPAR A TELA QUANDO PRECIONADO A TECLA 'PageUp' DO TECLADO:
-document.body.addEventListener('keydown', (event) => 
-    event.key === 'PageUp' && document.forms[0].reset()); 
-    //SÃO A MESMA COISA, POREM ESTE UTILIZA O CODIGO DA TABELA ASCII PARA ATIVAR 
-    /* events.keyCode  === 35 && document.getElementById('limpar').click() */ 
-    
-    // LIMPA APENAS O CAMPO DO SOMATORIO QUANDO PRECIONADO A TECLA 'PageDown'
-    document.body.addEventListener('keydown', (event) => 
-        event.key === 'PageDown' && (document.getElementById('soma_f').value = ''));
+
+    let seletor = document.getElementById("select")
+    let sIpi = 0;
+    let _Ipi = 0;
+    let _Icms = 0;
+    let j = 0;
 
 
-let i = true;
-document.getElementById('botao').addEventListener('click', () => {
-    if (i == false) {
-        document.getElementById('tabela').innerHTML = " ";
+function calcular_final() {
+    let posicao = document.getElementById("posicao");
+    let soma_semIpi = document.getElementById("soma_f");
+    let soma_comIpi = document.getElementById("soma_f_comIpi");
+    let soma_ipi = document.getElementById("som_ipi");
+    let soma_icms = document.getElementById("som_icms");
+    let redu_pis_cofins = document.getElementById("soma_pis_cofins");
+    let redu_pis = document.getElementById("soma_pis");
+    let redu_cofins = document.getElementById("soma_cofins");
+    let Icms = parseFloat(document.getElementById("icms").value);
+    let Ipi = parseFloat(document.getElementById("ipi").value);
+    let quant_soma = parseInt(document.getElementById("quantidade_soma").value);
+    let pesoSemIpi = parseFloat(document.getElementById('peso_total').value);
+    if(j < quant_soma){
+        if(seletor.value == '21'|| seletor.value == '22'|| seletor.value == '20' || seletor.value == '02'){
+        sIpi += pesoSemIpi;
+            if(seletor.value == '21'|| seletor.value == '22'|| seletor.value == '20'){
+                 _Ipi += Ipi; 
+                soma_ipi.value = _Ipi.toFixed(2);
+                
+                if(seletor.value == '21'){
+                    _Icms += Icms;
 
-        let tabela = document.getElementById('botao').value = "ABRIR";
-    } else {
-        document.getElementById('tabela').innerHTML = "<tr><td><strong>LETRA</strong></td><td><strong>CATEGORIA</strong></td><td><strong>NCM</strong></td></tr><tr><td><strong>A</strong></td><td>BUCHA BRONZE</td><td>74072921</td></tr><tr><td><strong>B</strong></td><td>TARUGO DE BRONZE</td><td>74072910</td></tr><tr><td><strong>C</strong></td><td>BARRA LATAO</td><td>74072110</td></tr><tr><td><strong>D</strong></td><td>CHAPA LATAO</td><td>74092900</td></tr><tr><td><strong>F</strong></td><td>BARRA COBRE</td><td>74071010</td></tr><tr><td><strong>G</strong></td><td>CHAPA COBRE</td><td>74091900</td></tr><tr><td><strong>H</strong></td><td>TUBO COBRE</td><td>74111010</td></tr><tr><td><strong>I</strong></td><td>BARRA ALUMINIO</td><td>76042919</td></tr><tr><td><strong>J</strong></td><td>CHAPA ALUMINIO</td><td>76061290</td></tr><tr><td><strong>L</strong></td><td>TARUGO ALUMINIO</td><td>76012000</td></tr><tr><td><strong>K</strong></td><td>TUBO ALUMINIO</td><td>76082090</td></tr><tr><td><strong>M</strong></td><td>CHAPA INOX</td><td>72193300</td></tr><tr><td><strong>N</strong></td><td>BARRA INOX</td><td>72221100</td></tr><tr><td><strong>O</strong></td><td>CHUMBO BRIDGESTONE</td><td>78011011</td></tr>";
-        document.getElementById('botao').value = 'ESCONDER';
+                }else if(seletor.value == '22' || seletor.value == '20'){
+                     let Redu = Icms;
+                     let Redu2 = (Redu - (Icms * 0.3333)).toFixed(2);
+                    _Icms += parseFloat(Redu2);
+                }
+
+            } else if(seletor.value == '02'){
+                 _Ipi += 0;
+                soma_ipi.value = _Ipi;
+                 _Icms += Icms;
+            }
+
+            j++;
+        }
+         soma_semIpi.value = sIpi.toFixed(2);
+         soma_icms.value = ((_Icms).toFixed(2))
+         soma_comIpi.value = ((sIpi + _Ipi).toFixed(2));
+         redu_pis_cofins.value = ((sIpi - _Icms).toFixed(2));
+         redu_pis.value = (((sIpi - _Icms)*0.0065).toFixed(2));
+         redu_cofins.value = (((sIpi - _Icms)*0.03).toFixed(2));
+         document.getElementById('posicao').textContent = `CALCULADORA TOTAL - Posição ${j}` 
+         posicao.value = j;
+
     }
-    i = !i; //alterna o valor entre verdadiro e falso
+ 
 
+
+}
+
+ let abrir = false;
+
+    document.getElementById('_calc').addEventListener('click', () => {
+    if(abrir == true){
+        document.getElementById('calc_total').style.display = "block";
+        abrir = false
+    } else{
+        document.getElementById('calc_total').style.display = "none";
+        abrir = true
+    }
 });
-
